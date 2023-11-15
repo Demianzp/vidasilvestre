1aefe37a411af3523b2a40067f6d6bcbd7344b26
Demian <demianagustinds3@gmail.com>
2023-11-15 11:49:21 -0300

    Inscribir actualizado

--- insertar.php
+++ insertar.php
@@ -21,23 +21,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id_personas = explode(",", $_POST['id_persona']);
         $id_materias = $_POST['id_materia'];
 
-        // Verificar el botón presionado
-        $accion = $_POST['accion'];
+        // Procesar la inserción en la base de datos
+        foreach ($id_personas as $id_persona) {
+            foreach ($id_materias as $id_materia) {
+                // Verificar si la materia existe antes de insertar en estadoalumno
+                $verificar_materia = "SELECT id_materia FROM materia WHERE id_materia = '$id_materia'";
+                $result_verificar_materia = $conn->query($verificar_materia);
 
-        // Procesar la acción correspondiente
-        switch ($accion) {
-            case 'Inscribir':
-                // Tu lógica para la inscripción
-                break;
+                if ($result_verificar_materia->num_rows > 0) {
+                    // Query de inserción en estadoalumno
+                    $sql = "INSERT INTO estadoalumno (id_persona, id_materia) VALUES ('$id_persona', '$id_materia')";
 
-            case 'Cancelar':
-                // Tu lógica para la cancelación
-                // Puedes cambiar el estado de la inscripción en tu base de datos
-                break;
-
-            default:
-                // Acción no reconocida
-                break;
+                    // Ejecutar la consulta
+                    if ($conn->query($sql) === TRUE) {
+                        $mensaje = "Carga exitosa";
+                        header("Location: seleccionar_alumnos.php?mensaje=" . urlencode($mensaje));
+                        exit();
+                    } else {
+                        $error = "Error: " . $conn->error;
+                        header("Location: seleccionar_alumnos.php?error=" . urlencode($error));
+                        exit();
+                    }
+                } else {
+                    $error = "Error: La materia con id_materia = $id_materia no existe en la tabla materia.";
+                    header("Location: seleccionar_alumnos.php?error=" . urlencode($error));
+                    exit();
+                }
+            }
         }
 
         // Cerrar la conexión
@@ -54,4 +64,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 ?>
-
