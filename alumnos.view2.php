5ea4a6da1f45327e788f75dac9cdfc1f75767b8d
Demian <demianagustinds3@gmail.com>
2023-11-16 20:30:41 -0300

    add

--- alumnos.view2.php
+++ alumnos.view2.php
@@ -19,14 +19,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fecha_nacimiento = isset($_POST["fecha_nacimiento"]) ? $_POST["fecha_nacimiento"] : '';
     $fecha_ingreso = isset($_POST["fecha_ingreso"]) ? $_POST["fecha_ingreso"] : '';
     $contrasena = isset($_POST["contrasena"]) ? $_POST["contrasena"] : '';
-    
+
     // Definir el valor predeterminado para el campo "estado" (asumiendo que se llama "estado")
     $estado = "Activo";
-    $pais="Argentina";
+    $pais = "Argentina";
     try {
         $sql = "INSERT INTO persona (nombre, apellido, fecha_nacimiento, DNI, celular, email_correo, direccion, fecha_ingreso, pais, ciudad, contraseña, id_rol, genero, estado) 
         VALUES (:nombre, :apellido, :fecha_nacimiento, :dni, :celular, :email, :direccion, :fecha_ingreso, :pais, :ciudad, :contrasena, :id_rol, :genero, :estado)";
-        
+
         $stmt = $db->prepare($sql);
         // Vincular los parámetros
         $stmt->bindParam(':nombre', $nombre);
@@ -50,11 +50,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $error = "Error al ingresar Persona: " . $stmt->errorInfo()[2];
         }
-
     } catch (PDOException $e) {
         $error = "Error en la consulta: " . $e->getMessage();
     }
 }
 // Redirigir a la página "listadoalumnos.view.php" con los mensajes en la URL
 header("Location: listadoalumnos.view.php?mensaje=" . urlencode($mensaje) . "&error=" . urlencode($error));
-exit();?>
+exit();
