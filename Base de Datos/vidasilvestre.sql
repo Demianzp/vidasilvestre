3e78a359666069c7f848faa8dee75b105468d9db
Demian <demianagustinds3@gmail.com>
2024-09-25 15:17:38 -0300

    modificacion segun lo que pidieron

--- Base de Datos/vidasilvestre.sql
+++ Base de Datos/vidasilvestre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 31-07-2024 a las 23:27:05
+-- Tiempo de generación: 25-09-2024 a las 20:04:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -52,6 +52,16 @@ CREATE TABLE `alumno_materia` (
   `fecha_inscripcion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `alumno_materia`
+--
+
+INSERT INTO `alumno_materia` (`Id_alumno`, `id_persona`, `id_materia`, `id_ciclo`, `id_nota`, `cod_correlativa`, `estado`, `fecha_inscripcion`) VALUES
+(1, 7, 1, 3, NULL, NULL, 'Inscripto', '2024-09-25 17:57:40'),
+(2, 7, 3, 3, NULL, NULL, 'Inscripto', '2024-09-25 18:12:46'),
+(3, 2, 3, 1, NULL, NULL, 'Inscripto', '2024-09-25 19:58:11'),
+(4, 7, 4, 1, NULL, NULL, 'Inscripto', '2024-09-25 19:58:58');
+
 -- --------------------------------------------------------
 
 --
@@ -84,6 +94,15 @@ CREATE TABLE `ciclo_lectivo` (
   `ciclo_actual` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `ciclo_lectivo`
+--
+
+INSERT INTO `ciclo_lectivo` (`id_ciclo`, `nombre_ciclo`, `fecha_inicio`, `fecha_fin`, `Estado`, `created_at`, `updated_at`, `ciclo_actual`) VALUES
+(1, '2024', '2024-06-19', '2024-06-26', 'Activo', '2024-06-08 13:24:14', '2024-09-25 16:24:16', 1),
+(2, '2025', '2024-09-25', '2024-09-17', 'Activo', '2024-09-14 15:43:44', '2024-09-14 15:43:44', 0),
+(3, '2026', '2024-09-22', '2024-09-13', 'Activo', '2024-09-22 05:29:26', '2024-09-25 16:24:16', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +115,26 @@ CREATE TABLE `correlativa` (
   `id_correlativa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `correlativa`
+--
+
+INSERT INTO `correlativa` (`cod_correlativa`, `id_materia`, `id_correlativa`) VALUES
+(1, 6, 5),
+(2, 7, 2),
+(3, 7, 5),
+(4, 8, 2),
+(5, 9, 3),
+(6, 9, 4),
+(7, 10, 2),
+(8, 10, 3),
+(9, 10, 4),
+(10, 10, 5),
+(11, 10, 6),
+(12, 10, 7),
+(13, 10, 8),
+(14, 10, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -104,8 +143,28 @@ CREATE TABLE `correlativa` (
 
 CREATE TABLE `examen` (
   `id_examen_tipo` int(11) NOT NULL,
-  `nombre_examen` varchar(255) NOT NULL
+  `nombre_examen` varchar(255) NOT NULL,
+  `tipo` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `examen`
+--
+
+INSERT INTO `examen` (`id_examen_tipo`, `nombre_examen`, `tipo`) VALUES
+(1, 'Nota1', 'regular'),
+(2, 'Nota2', 'regular'),
+(3, 'Nota3', 'regular'),
+(4, 'Nota4', 'regular'),
+(5, 'Calif. Regular', 'mostrar'),
+(6, 'Calif. 1º Ex. Final', 'final'),
+(7, 'Calif. 2º Ex. Final', 'final'),
+(8, 'Calif. Final', 'final'),
+(9, '1º Per. Ev. Dic.', 'final'),
+(10, '2º Per. Ev. Dic.', 'final'),
+(11, '1º Per. Ev. Feb.', 'final'),
+(12, '2º Per. Ev. Feb.', 'final'),
+(13, 'Calificación Definitiva', 'mostrar');
 
 -- --------------------------------------------------------
 
@@ -115,25 +174,31 @@ CREATE TABLE `examen` (
 
 CREATE TABLE `materia` (
   `id_materia` int(11) NOT NULL,
-  `Nombre` varchar(15) DEFAULT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
   `descripcion` varchar(50) DEFAULT NULL,
   `horas` varchar(5) DEFAULT NULL,
-  `num_resolucion` varchar(11) DEFAULT NULL,
-  `plan_estudio` varchar(11) DEFAULT NULL,
+  `num_resolucion` varchar(15) DEFAULT NULL,
   `año_cursado` varchar(5) NOT NULL,
   `id_tipo` int(11) DEFAULT NULL,
-  `estado` varchar(11) DEFAULT NULL
+  `estado` varchar(11) DEFAULT NULL,
+  `plan_estudio` enum('1° Cuatrimestre','2° Cuatrimestre') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `materia`
 --
 
-INSERT INTO `materia` (`id_materia`, `Nombre`, `descripcion`, `horas`, `num_resolucion`, `plan_estudio`, `año_cursado`, `id_tipo`, `estado`) VALUES
-(1, 'lengua', 'd2', '2', '3', '1', '1', 2, 'Activo'),
-(2, 'Matematica', '---', '3', 'AABS12', '1', '1', 1, 'Activo'),
-(3, 'Progrmacion', '--', '2', 'asgaas', '1', '1', 1, 'Activo'),
-(4, 'SOYR', '--', '3', 'AASGX12', '2', '1', 1, 'Activo');
+INSERT INTO `materia` (`id_materia`, `Nombre`, `descripcion`, `horas`, `num_resolucion`, `año_cursado`, `id_tipo`, `estado`, `plan_estudio`) VALUES
+(1, 'Lengua', '--', '3', 'ASD23', '1', 2, 'Inactivo', '1° Cuatrimestre'),
+(2, 'Compresión y Producción de texto', 'CyPT', '3', 'ASD123', '1', 2, 'Activo', '1° Cuatrimestre'),
+(3, 'Biologia', '--', '3', 'ASD123', '1', 2, 'Activo', '1° Cuatrimestre'),
+(4, 'Ecologia', '--', '3', 'ASD123', '1', 2, 'Activo', '1° Cuatrimestre'),
+(5, 'Contexto socioeconomico ambiental', '--', '3', 'ASD123', '1', 2, 'Activo', '1° Cuatrimestre'),
+(6, 'Informatica Aplicada', '--', '3', 'ASD123', '2', 2, 'Activo', '2° Cuatrimestre'),
+(7, 'Educación Ambiental 1', '--', '3', 'ASD123', '2', 2, 'Activo', '2° Cuatrimestre'),
+(8, 'Geografia Regional', '--', '2', 'asd123', '1', 2, 'Activo', '2° Cuatrimestre'),
+(9, 'Prevencion y Manejo de Fuego en áreas protegidas ', '--', '2', 'ASD123', '1', 2, 'Activo', '2° Cuatrimestre'),
+(10, 'Practicas Profesionalizantes 1', '--', '3', 'ASD123', '1', 2, 'Activo', '2° Cuatrimestre');
 
 -- --------------------------------------------------------
 
@@ -164,8 +229,20 @@ CREATE TABLE `nota` (
   `id_persona` int(11) NOT NULL,
   `id_materia` int(11) NOT NULL,
   `id_ciclo` int(11) NOT NULL,
-  `id_examen_tipo` int(11) NOT NULL,
-  `nota` float NOT NULL
+  `n1` float DEFAULT NULL,
+  `n2` float DEFAULT NULL,
+  `n3` float DEFAULT NULL,
+  `n4` float DEFAULT NULL,
+  `n5` float DEFAULT NULL,
+  `n6` float DEFAULT NULL,
+  `n7` float DEFAULT NULL,
+  `n8` float DEFAULT NULL,
+  `n9` float DEFAULT NULL,
+  `n10` float DEFAULT NULL,
+  `n11` float DEFAULT NULL,
+  `n12` float DEFAULT NULL,
+  `n13` float DEFAULT NULL,
+  `estado` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -193,6 +270,17 @@ CREATE TABLE `persona` (
   `titulo` varchar(100) DEFAULT NULL,
   `estado` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id_persona`, `nombre`, `apellido`, `fecha_nacimiento`, `DNI`, `celular`, `email_correo`, `direccion`, `fecha_ingreso`, `pais`, `ciudad`, `contraseña`, `id_rol`, `genero`, `legajo`, `titulo`, `estado`) VALUES
+(2, 'Luis', 'Mercado', '2014-06-20', 44231781, '26', 'aadminjuan@gmail.com', '9 de julio y san juan', '2024-06-12', 'Argentina', 'Angaco', '123', 1, 'Masculino', NULL, NULL, 'Activo'),
+(3, 'Facundo', 'Ramirez', '2014-06-20', 44231782, '26', 'aaadminjuan@gmail.com', '9 de julio y san juan', '2024-06-12', 'Argentina', 'Angaco', '123', 1, 'Masculino', NULL, NULL, 'Activo'),
+(7, 'Juan', 'Perez', '2014-06-20', 44231783, '26', 'adminjuan@gmail.com', '9 de julio y san juan', '2024-06-12', 'Argentina', 'Angaco', '123', 1, 'Masculino', NULL, NULL, 'Activo'),
+(9, 'maxi', 'olmos', '0000-00-00', 0, NULL, 'm@gmail.com', NULL, NULL, NULL, NULL, '123', 3, NULL, NULL, NULL, 'Activo'),
+(13, 'Demi', 'Perez', '2014-06-20', 44231783, '26', 'dem23@gmail.com', '9 de julio y san juan', '2024-06-12', 'Argentina', 'Angaco', '123456', 2, 'Masculino', 2332423, 'Preceptor', 'Activo');
 
 -- --------------------------------------------------------
 
@@ -308,8 +396,7 @@ ALTER TABLE `nota`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_nota_persona` (`id_persona`),
   ADD KEY `fk_nota_materia` (`id_materia`),
-  ADD KEY `fk_nota_ciclo` (`id_ciclo`),
-  ADD KEY `fk_nota_examen` (`id_examen_tipo`);
+  ADD KEY `fk_nota_ciclo` (`id_ciclo`);
 
 --
 -- Indices de la tabla `persona`
@@ -344,7 +431,7 @@ ALTER TABLE `acta`
 -- AUTO_INCREMENT de la tabla `alumno_materia`
 --
 ALTER TABLE `alumno_materia`
-  MODIFY `Id_alumno` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `asignar`
@@ -356,25 +443,25 @@ ALTER TABLE `asignar`
 -- AUTO_INCREMENT de la tabla `ciclo_lectivo`
 --
 ALTER TABLE `ciclo_lectivo`
-  MODIFY `id_ciclo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ciclo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `correlativa`
 --
 ALTER TABLE `correlativa`
-  MODIFY `cod_correlativa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_correlativa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `examen`
 --
 ALTER TABLE `examen`
-  MODIFY `id_examen_tipo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_examen_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `mesa_examen`
@@ -392,7 +479,7 @@ ALTER TABLE `nota`
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -460,7 +547,6 @@ ALTER TABLE `mesa_examen`
 --
 ALTER TABLE `nota`
   ADD CONSTRAINT `fk_nota_ciclo` FOREIGN KEY (`id_ciclo`) REFERENCES `ciclo_lectivo` (`id_ciclo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_nota_examen` FOREIGN KEY (`id_examen_tipo`) REFERENCES `examen` (`id_examen_tipo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_nota_materia` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_nota_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
